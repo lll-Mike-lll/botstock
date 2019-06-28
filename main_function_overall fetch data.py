6686b5a32dd83221a5165a551cc350784942b28b
lll-Mike-lll <wuttinun.code@gmail.com>
2019-06-28 11:29:03 +0700

    m

---
+++ main_function_overall fetch data.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 10:57:43 2019
+
+@author: Lenovo
+"""
+import pandas as pd
+import pymongo as pm
+cli = pm.MongoClient()
+db = cli['stock']
+coll = db['stock_list']
+
+data2 = coll.find()
+name = []
+for n in data2:
+    name.append(n['stock_name'])
+
+print('a')
+
+db2 = cli['stock7']
+kk = 0
+
+for j in range(len(name)):
+    web_name = 'http://www.panphol.com/data/page/stockprice/'+name[j]
+    data = pd.read_html(web_name)
+    print('e')
+    for i1 in range(len(data[0])):
+        coll2 = db2[name[j]]
+        #reverse order older to recently
+        i=len(data[0])-1-i1
+        data1 = {'date':data[0].iloc[i,0],
+             'open':data[0].iloc[i,1],
+             'max':data[0].iloc[i,2],
+             'min':data[0].iloc[i,3],
+             'close':data[0].iloc[i,4],
+             'change':data[0].iloc[i,5],
+             'percent chg':data[0].iloc[i,6],
+             'volume':int(data[0].iloc[i,7]),
+             'value':int(data[0].iloc[i,8])}
+        coll2.insert_one(data1)
+    kk=kk+1
+    print('b')
+    print(kk/len(name)*100)
+    
+print('c')
--- try_32_fetch and order data.py
+++ try_32_fetch and order data.py
@@ -25,39 +25,71 @@ for n in data2:
                  n['percent chg'],
                  n['volume'],
                  n['value']])
-print(name)
-print(name3)
+#print(name)
+#print(name3)
+    
+    
+print(name[0][0:2])
+print(name[0][3:5])
+print(name[0][6:10])
+
+a = int(name[0][0:2])
+print(a-20)
+
+#lately to older
+for j in range(0,len(name)-1):
+    for i in range(0,len(name)-1):
+        a1 = int(name[i][0:2])
+        a2 = int(name[i][3:5])
+        a3 = int(name[i][6:10])
+        b1 = int(name[i+1][0:2])
+        b2 = int(name[i+1][3:5])
+        b3 = int(name[i+1][6:10])
+        if a3<b3:
+            move = name[i]
+            name[i] = name[i+1]
+            name[i+1] = move
+        elif a3==b3:
+            if a2<b2:
+                move = name[i]
+                name[i] = name[i+1]
+                name[i+1] = move
+            elif a2==b2:
+                if a1<b1:
+                    move = name[i]
+                    name[i] = name[i+1]
+                    name[i+1] = move
+
+#print(name)
+
+#insert ordered data to list
+kk=0
+for i1 in range(10):
+    for j1 in range(len(name3)):
+        kk=kk+1
+#        if name[i1]==name3[j1][0]:
+#            name2.append(name3[j1])
+
+print(len(name3))
+#print(len(name))
+#print(len(name3))
+#print(name3[0])
+#print(name2)   
+#print(len(name2))
+
 # =============================================================================
-# print(name[0][0:2])
-# print(name[0][3:5])
-# print(name[0][6:10])
-# 
-# a = int(name[0][0:2])
-# print(a-20)
-# 
-# #lately to older
-# for j in range(0,len(name)-1):
-#     for i in range(0,len(name)-1):
-#         a1 = int(name[i][0:2])
-#         a2 = int(name[i][3:5])
-#         a3 = int(name[i][6:10])
-#         b1 = int(name[i+1][0:2])
-#         b2 = int(name[i+1][3:5])
-#         b3 = int(name[i+1][6:10])
-#         if a3<b3:
-#             move = name[i]
-#             name[i] = name[i+1]
-#             name[i+1] = move
-#         elif a3==b3:
-#             if a2<b2:
-#                 move = name[i]
-#                 name[i] = name[i+1]
-#                 name[i+1] = move
-#             elif a2==b2:
-#                 if a1<b1:
-#                     move = name[i]
-#                     name[i] = name[i+1]
-#                     name[i+1] = move
-# 
-# print(name)
+# db55 = cli['stock4']
+# coll55 = db55['AAV']
+# #table1 = ['date','open','max','min','close','change','percent chg','volume','value']
+# for i in range(len(name2)):
+#     data1 = {'date':name2[i][0],
+#              'open':name2[i][1],
+#              'max':name2[i][2],
+#              'min':name2[i][3],
+#              'close':name2[i][4],
+#              'change':name2[i][5],
+#              'percent chg':name2[i][6],
+#              'volume':int(name2[i][7]),
+#              'value':int(name2[i][8])}
+#     coll55.insert_one(data1)       
 # =============================================================================
